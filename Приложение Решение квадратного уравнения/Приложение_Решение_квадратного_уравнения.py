0256f8e2b48858d992cf65c922de93da858cfea9
opilane <opilane@HP-CZC6297B82>
2022-01-07 11:58:51 +0200

    ма

--- Приложение Решение квадратного уравнения/Приложение_Решение_квадратного_уравнения.py
+++ Приложение Решение квадратного уравнения/Приложение_Решение_квадратного_уравнения.py
@@ -1,15 +1,33 @@
 from tkinter import*
-klik=0
-def klikker(event):
-    global klik
+from math import *
+
+    
 
 aken=Tk()
 aken.title("Квадратные уровнения")
-aken.geometry("600x300")
+aken.geometry("620x200")
 
-nupp=Button(aken,text="Решить",font="Arial 20",fg="black",bg="green",height=2,width=10,relief=GROOVE)#RAISED, SUNKEN
-nupp.bind("<Button-1>",klikker)
 
-nupp.pack(side=RIGHT)#side=LEFT,TOP,RIGHT
+lbl=Label(aken,text="Решение квадратного уравнения",font="Calibri 26",fg="green",bg="lightblue",justify=CENTER)
+lbl.pack()
+sutsav=Label(aken,text="Ответ",heigh=1,width=60,font="Calibri 26",fg="green",bg="yellow")
+sutsav.pack(side=BOTTOM)
+a=Entry(aken,width=3,font="Arial 20",fg="green",bg="lightblue")
+a.pack(side=LEFT)
+x1=Label(aken,text="x**2+",font="Calibri 26",fg="green")
+x1.pack(side=LEFT)
+b=Entry(aken,width=3,font="Arial 20",fg="green",bg="lightblue")
+b.pack(side=LEFT)
+x2=Label(aken,text="x+",font="Calibri 26",fg="green")
+x2.pack(side=LEFT)
+c=Entry(aken,width=3,font="Arial 20",fg="green",bg="lightblue")
+c.pack(side=LEFT)
+o=Label(aken,text="=0",font="Calibri 26",fg="green")
+o.pack(side=LEFT)
+
+vastus=Button(aken,text="Решить",font="Arial 20",fg="black",bg="green",height=2,width=5,relief=GROOVE)#RAISED, SUNKEN
+vastus.pack(side=RIGHT)
+grafik=Button(aken,text="График",font="Arial 20",fg="black",bg="green",height=2,width=5,relief=GROOVE)#RAISED, SUNKEN
+grafik.pack(side=RIGHT)
 
 aken.mainloop()

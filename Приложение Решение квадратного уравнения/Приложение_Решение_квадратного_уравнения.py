a459532766b6b10339efbeab3a977e0515c98a3a
opilane <opilane@HP-CZC6297B82>
2022-01-11 11:46:11 +0200

    ........................................................................................................................................................................................................................................................................................................................

--- Приложение Решение квадратного уравнения/Приложение_Решение_квадратного_уравнения.py
+++ Приложение Решение квадратного уравнения/Приложение_Решение_квадратного_уравнения.py
@@ -1,34 +1,23 @@
-from tkinter import*#Имортируем это для окна
+from tkinter import*
 from math import *
-def lol():#Это все для одной кнопки
-    global a,b,c
+import matplotlib.pyplot as plt
+import numpy as np
+
+def lol(event):    
     if (a.get()!="" and b.get()!="" and c.get()!=""):
         a_=int(a.get())
         b_=int(b.get())
         c_=int(c.get())
         D=b_*b_-4*a_*c_
-        if (a.get()==0 and b.get()==0 and c.get()==0):
-            sutsav.configure(text=f"D={D}\n{t}")
-            a.configure(bg="red")#Если будут одни 0, то эти окна будут красними
-            b.configure(bg="red")#Если будут одни 0, то эти окна будут красними
-            c.configure(bg="red")#Если будут одни 0, то эти окна будут красними
-            if D>0:
-                x1_=round((-1*b_+sqrt(D))/(2*a_),2)#Это решение
-                x2_=round((-1*b_-sqrt(D))/(2*a_),2)#Это решение
-                t=f"X1={x1_}, \nX2={x2_}"
-                flag=True
-            elif D==0:
-                x1_=round((-1*b_)/(2*a_),2)#Это решение
-                t=f"X1={x1_}"
-                flag=True
-            elif a==0:
-                t="Корней нет"
-                flag=False
-                a.configure(bg="lightblue")
-            elif b==0:
-                b_=round((c_/b_),2)
-                t=f"X1={b_}"
-                flag=True
+        if D>0:
+            x1_=round((-1*b_+sqrt(D))/(2*a_),2)
+            x2_=round((-1*b_-sqrt(D))/(2*a_),2)
+            t=f"X1={x1_}, \nX2={x2_}"
+            flag=True
+        elif D==0:
+            x1_=round((-1*b_)/(2*a_),2)
+            t=f"X1={x1_}"
+            flag=True
         else:
             t="Корней нет"
             flag=False
@@ -36,38 +25,74 @@ def lol():#Это все для одной кнопки
             a.configure(bg="lightblue")
             b.configure(bg="lightblue")
             c.configure(bg="lightblue")
+        return flag,D,t
     else:
         if a.get()=="":
-            a.configure(bg="red")#Если в окне не будет ничего написано, то окно будет красным
+            a.configure(bg="red")
         if b.get()=="":
-            b.configure(bg="red")#Если в окне не будет ничего написано, то окно будет красным
+            b.configure(bg="red")
         if c.get()=="":
-            c.configure(bg="red")#Если в окне не будет ничего написано, то окно будет красным
+            c.configure(bg="red")
+
+def kliik(event):
+    aken.geometry(str(aken.winfo_width()+10)+"x"+str(aken.winfo_height()+10))
+
+def kliikut(event):
+    aken.geometry(str(aken.winfo_width()-10)+"x"+str(aken.winfo_height()-10))
+
+def graafik():
+    flag,D,t=lol()
+    if flag==True:
+        a_=int(a.get())
+        b_=int(b.get())
+        c_=int(c.get())
+        x0=(-b_)/(2*a_)
+        y0=a_*x0*x0+b_*x0+c_
+        x = np.arange(x0-10, x0+10, 0.5)#min max step
+        y=a_*x*x+b_*x+c_
+        fig = plt.figure()
+        plt.plot(x, y,'b:o', x0, y0,'g-d')
+        plt.title('Квадратное уравнение')
+        plt.ylabel('y')
+        plt.xlabel('x')
+        plt.grid(True)
+        plt.show()
+        text=f"Вершина параболлы ({x0},{y0})"
+    else:
+        text=f"График нет возможности построить"
+    sutsav.configure(text=f"D={D}\n{t}\n{text}")
 
 aken=Tk()
-aken.title("Квадратные уравнения")#название окна
-aken.geometry("620x200")#размер окна
+aken.title("Квадратные уравнения")
+aken.geometry("620x350")
 
-lbl=Label(aken,text="Решение квадратного уравнения",font="Calibri 26",fg="green",bg="lightblue",justify=CENTER)#Здесь делаем вид и чтобы мы могли видеть что написано, но не менять, команда Label в этом нам помогает
+lbl=Label(aken,text="Решение квадратного уравнения",font="Calibri 26",fg="green",bg="lightblue",justify=CENTER)
 lbl.pack()
-sutsav=Label(aken,text="Ответ",width=60,font="Calibri 26",fg="green",bg="yellow")#Здесь делаем вид и чтобы мы могли видеть что написано, но не менять, команда Label в этом нам помогает
-sutsav.pack(side=BOTTOM)#Это для место положения надписи, ну и срабатывании кнопки
-a=Entry(aken,width=3,font="Arial 20",fg="green",bg="lightblue")#Здесь делаем вид и чтобы мы могли что нибудь в этом окне написать что нибудь, команда Entry в этом нам помогает
-a.pack(side=LEFT)#Это для место положения надписи, ну и срабатывании кнопки
-x1=Label(aken,text="x**2+",font="Calibri 26",fg="green")#Здесь делаем вид и чтобы мы могли видеть что написано, но не менять, команда Label в этом нам помогает
-x1.pack(side=LEFT)#Это для место положения надписи, ну и срабатывании кнопки
-b=Entry(aken,width=3,font="Arial 20",fg="green",bg="lightblue")#Здесь делаем вид и чтобы мы могли что нибудь в этом окне написать что нибудь, команда Entry в этом нам помогает
-b.pack(side=LEFT)#Это для место положения надписи, ну и срабатывании кнопки
-x2=Label(aken,text="x+",font="Calibri 26",fg="green")#Здесь делаем вид и чтобы мы могли видеть что написано, но не менять, команда Label в этом нам помогает
-x2.pack(side=LEFT)#Это для место положения надписи, ну и срабатывании кнопки
-c=Entry(aken,width=3,font="Arial 20",fg="green",bg="lightblue")#Здесь делаем вид и чтобы мы могли что нибудь в этом окне написать что нибудь, команда Entry в этом нам помогает
-c.pack(side=LEFT)#Это для место положения надписи, ну и срабатывании кнопки
-o=Label(aken,text="=0",font="Calibri 26",fg="green")#Здесь делаем вид и чтобы мы могли видеть что написано, но не менять, команда Label в этом нам помогает
-o.pack(side=LEFT)#Это для место положения надписи, ну и срабатывании кнопки
+sutsav=Label(aken,text="Ответ",width=60,font="Calibri 26",fg="green",bg="yellow")
+sutsav.pack(side=BOTTOM)
+popa=Button(aken,text="Увеличить окно",height=1,width=15,font="Calibri 26",fg="black",bg="green")
+popa.pack(side=BOTTOM)
+popa.bind("<Button-1>",kliik)
+apop=Button(aken,text="Уменьшить окно",height=1,width=15,font="Calibri 26",fg="black",bg="green")
+apop.pack(side=BOTTOM)
+apop.bind("<Button-1>",kliikut)
+a=Entry(aken,width=3,font="Arial 20",fg="green",bg="lightblue")
+a.pack(side=LEFT)
+x1=Label(aken,text="x**2+",font="Calibri 26",fg="green") 
+x1.pack(side=LEFT)
+b=Entry(aken,width=3,font="Arial 20",fg="green",bg="lightblue")
+b.pack(side=LEFT)
+x2=Label(aken,text="x+",font="Calibri 26",fg="green")
+x2.pack(side=LEFT)
+c=Entry(aken,width=3,font="Arial 20",fg="green",bg="lightblue")
+c.pack(side=LEFT)
+o=Label(aken,text="=0",font="Calibri 26",fg="green")
+o.pack(side=LEFT)
 
-btn=Button(aken,text="Решить", font="Calibri 26",bg="green",command=lol)#Тут делаем вид кнопки и её применение
-btn.pack(side=LEFT)#Это для место положения надписи, ну и срабатывании кнопки
-grafik=Button(aken,text="График",font="Arial 20",fg="black",bg="green",relief=GROOVE) #Тут делаем вид кнопки и её применение
-grafik.pack(side=RIGHT)#Это для место положения надписи, ну и срабатывании кнопки
+btn=Button(aken,text="Решить", font="Calibri 26",bg="green",relief=GROOVE, command=lol)
+btn.pack(side=RIGHT)
+btn.bind("<Button-1>",lol)
+grafik=Button(aken,text="График",font="Calibri 26",fg="black",bg="green",relief=GROOVE,command=graafik) 
+grafik.pack(side=RIGHT)
 
-aken.mainloop()#Я не помню чо это, но это нужно для окна
+aken.mainloop()
